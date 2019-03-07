a92e63dd3aaae08f3b02128434ecd49bfd4e5984
Lidii <lidiiany92@gmail.com>
2019-03-07 15:14:24 -0500

    Initialized the variables for in the app.js

--- app/app.js
+++ app/app.js
@@ -8,3 +8,21 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+//Random number from 1 to 6
+dice = Math.floor(Math.random() * 6) + 1;
+
+//Current dice roll number
+const currentScore = document.querySelector(`#current-${activePlayer}`);
+currentScore.textContent = dice;
+
+//Hiding dice img initialy on a page load
+const diceImg = document.querySelector('.dice');
+diceImg.style.display = 'none';
+
